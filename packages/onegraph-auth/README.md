755ad341f53e61a83d54c1a472cc8d61456d46d5
Sean Grove <sean@bushi.do>
2019-09-19 18:01:30 -0700

    Progressive authentication helper (#16)
    * Add findMissingAuthServices helper to onegraph-auth
    * Slightly touch up the onegraph-auth README

--- packages/onegraph-auth/README.md
+++ packages/onegraph-auth/README.md
@@ -1,16 +1,3 @@
 ## OneGraph Auth
 
-### Supported services
-
-  - eventil
-  - github
-  - gmail
-  - google
-  - google-compute
-  - google-docs
-  - salesforce
-  - stripe
-  - twilio
-  - twitter
-  - youtube
-  - zendesk
+The main helper library for automating all-things authentication in the browser. Many services are supported for logging in/out of applications, and more details about the library can be found on [the OneGraph docs site](https://www.onegraph.com/docs/authentication.html).
--- packages/onegraph-auth/src/auth.js
+++ packages/onegraph-auth/src/auth.js
@@ -395,6 +395,37 @@ function tokenFromStorage(storage: Storage, appId: string): ?Token {
   return null;
 }
 
+function findMissingAuthServices(results) {
+  /* Detect and normalize between:
+  1. The full graphql result
+  2. The `result.errors` of a graphql result
+  3. Apollo's GraphQL error structure
+   */
+  let errors =
+    results &&
+    // Full GraphQL result
+    (results.errors ||
+      // Apollo error
+      results.graphQLErrors ||
+      // Possibly result.errors
+      results);
+
+  // If errors aren't an array, bail
+  if (!Array.isArray(errors)) {
+    return [];
+  }
+
+  const missingServiceErrors = errors.filter(
+    error => idx(error, _ => _.extensions.type) === 'auth/missing-auth',
+  );
+
+  const missingServices = missingServiceErrors
+    .map(error => idx(error, _ => _.extensions.service))
+    .filter(Boolean);
+
+  return missingServices;
+}
+
 const DEFAULT_ONEGRAPH_ORIGIN = 'https://serve.onegraph.com';
 
 class OneGraphAuth {
@@ -799,6 +830,8 @@ class OneGraphAuth {
     Object.keys(this._authWindows).forEach(key => this.cleanup(key));
     this._storage.removeItem(this._storageKey);
   };
+
+  findMissingAuthServices = findMissingAuthServices;
 }
 
 export default OneGraphAuth;
