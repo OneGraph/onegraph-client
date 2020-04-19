3d8b89deee9b3b2feb12b84f5d017a7826d4cb13
Daniel Woelfel <dwwoelfel@gmail.com>
2020-04-18 19:24:35 -0700

    update flow and rollup

--- rollup.js
+++ rollup.js
@@ -1,24 +1,30 @@
 const babel = require('rollup-plugin-babel');
 const postcss = require('rollup-plugin-postcss');
-const resolve = require('rollup-plugin-node-resolve');
+const resolve = require('@rollup/plugin-node-resolve');
+const commonjs = require('@rollup/plugin-commonjs');
+
 const rollup = require('rollup');
 
-async function buildForFormat(moduleName, format) {
-  const bundle = await rollup.rollup({
+function createBundle() {
+  return rollup.rollup({
     input: 'src/index.js',
     external: ['react'],
     plugins: [
-      resolve(),
       babel({
         exclude: 'node_modules/**',
-        presets: ['react', 'stage-0'],
+        presets: ['react', 'stage-0', 'flow'],
       }),
+      commonjs(),
+      resolve(),
+
       postcss({
         plugins: [],
       }),
     ],
   });
+}
 
+async function buildForFormat(bundle, moduleName, format) {
   await bundle.write({
     format,
     file: `dist/bundle.${format}.js`,
@@ -29,14 +35,16 @@ async function buildForFormat(moduleName, format) {
   });
 }
 
-module.exports = function build(moduleName) {
-  Promise.all([
-    buildForFormat(moduleName, 'umd'),
-    buildForFormat(moduleName, 'es'),
-  ])
-    .then(console.log(`finished building ${moduleName}`))
-    .catch(e => {
-      console.error(e);
-      process.exit(1);
-    });
+module.exports = async function build(moduleName) {
+  try {
+    const bundle = await createBundle();
+    await buildForFormat(bundle, moduleName, 'umd');
+    await buildForFormat(bundle, moduleName, 'es');
+    await buildForFormat(bundle, moduleName, 'cjs');
+
+    console.log(`finished building ${moduleName}`);
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
 };
