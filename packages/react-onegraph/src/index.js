fe80c8fe0a816b4a3716084fcdb5e1de056f30c6
Ian Jones <ian@egghead.io>
2020-05-09 09:22:15 -0400

    Add auth prop to support urql client

--- packages/react-onegraph/src/index.js
+++ packages/react-onegraph/src/index.js
@@ -1,39 +1,46 @@
-import React, {Component, createContext} from 'react';
-import OneGraphAuth from 'onegraph-auth';
+import React, {Component, createContext} from 'react'
+import OneGraphAuth from 'onegraph-auth'
 
-const AuthContext = createContext();
+const AuthContext = createContext()
 
 class AuthProvider extends Component {
   state = {
     auth: null,
     status: {},
     headers: {},
-  };
+  }
 
   componentDidMount() {
-    const auth = new OneGraphAuth({
-      appId: this.props.appId,
-    });
+    const auth = this.getAuth()
 
-    auth.servicesStatus().then(status =>
+    auth.servicesStatus().then((status) =>
       this.setState({
         headers: auth.authHeaders(),
         status: Object.keys(status).reduce((out, service) => {
-          out[service] = status[service].isLoggedIn;
-          return out;
+          out[service] = status[service].isLoggedIn
+          return out
         }, {}),
         auth,
       }),
-    );
+    )
+  }
+
+  getAuth = () => {
+    const auth =
+      this.props.auth ||
+      new OneGraphAuth({
+        appId: this.props.appId,
+      })
+    return auth
   }
 
   login = (service, callback) => {
-    const {auth, status, headers} = this.state;
+    const {auth, status} = this.state
 
     if (auth) {
       auth.login(service).then(() => {
-        auth.isLoggedIn(service).then(isLoggedIn => {
-          callback && callback(isLoggedIn);
+        auth.isLoggedIn(service).then((isLoggedIn) => {
+          callback && callback(isLoggedIn)
 
           this.setState({
             status: {
@@ -41,18 +48,18 @@ class AuthProvider extends Component {
               [service]: isLoggedIn,
             },
             headers: auth.authHeaders(),
-          });
-        });
-      });
+          })
+        })
+      })
     }
-  };
+  }
 
   logout = (service, callback) => {
-    const {auth, status, headers} = this.state;
+    const {auth, status} = this.state
 
     auth.logout(service).then(() => {
-      auth.isLoggedIn(service).then(isLoggedIn => {
-        callback && callback(isLoggedIn);
+      auth.isLoggedIn(service).then((isLoggedIn) => {
+        callback && callback(isLoggedIn)
 
         this.setState({
           status: {
@@ -60,14 +67,14 @@ class AuthProvider extends Component {
             [service]: isLoggedIn,
           },
           headers: auth.authHeaders(),
-        });
-      });
-    });
-  };
+        })
+      })
+    })
+  }
 
   render() {
-    const {appId, children} = this.props;
-    const {auth, status, headers} = this.state;
+    const {appId} = this.props
+    const {status, headers} = this.state
 
     const authInterface = {
       status,
@@ -75,16 +82,16 @@ class AuthProvider extends Component {
       login: this.login,
       logout: this.logout,
       appId,
-    };
+    }
 
     return (
       <AuthContext.Provider value={authInterface}>
         {this.props.children}
       </AuthContext.Provider>
-    );
+    )
   }
 }
 
-const AuthConsumer = AuthContext.Consumer;
+const AuthConsumer = AuthContext.Consumer
 
-export {AuthContext, AuthConsumer, AuthProvider};
+export {AuthContext, AuthConsumer, AuthProvider}
