fc346e281f8359a468cf7a4b3e9c69be1e102ff1
Sean Grove <sean@bushi.do>
2020-11-09 12:59:51 -0800

    Export findMissingAuthServices as a standalone helper

--- packages/onegraph-auth/src/index.js
+++ packages/onegraph-auth/src/index.js
@@ -1,5 +1,6 @@
 import OneGraphAuth from './auth';
 import {InMemoryStorage, LocalStorage} from './storage';
+import {findMissingAuthServices} from './helpers';
 
-export {OneGraphAuth, InMemoryStorage, LocalStorage};
+export {OneGraphAuth, InMemoryStorage, LocalStorage, findMissingAuthServices};
 export default OneGraphAuth;
