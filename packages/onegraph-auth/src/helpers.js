f25a505ee3d2b1f8bc066b2d24d74ca50a9050c4
Sean Grove <sean@bushi.do>
2020-11-09 13:16:05 -0800

    [onegraph-auth] Export find missing service (#40)
    * Export findMissingAuthServices as a standalone helper

---
+++ packages/onegraph-auth/src/helpers.js
@@ -0,0 +1,30 @@
+export function findMissingAuthServices(results: any) {
+  /* Detect and normalize between:
+    1. The full graphql result
+    2. The `result.errors` of a graphql result
+    3. Apollo's GraphQL error structure
+     */
+  let errors =
+    results &&
+    // Full GraphQL result
+    (results.errors ||
+      // Apollo error
+      results.graphQLErrors ||
+      // Possibly result.errors
+      results);
+
+  // If errors aren't an array, bail
+  if (!Array.isArray(errors)) {
+    return [];
+  }
+
+  const missingServiceErrors = errors.filter(
+    error => idx(error, _ => _.extensions.type) === 'auth/missing-auth',
+  );
+
+  const missingServices = missingServiceErrors
+    .map(error => idx(error, _ => _.extensions.service))
+    .filter(Boolean);
+
+  return missingServices;
+}
